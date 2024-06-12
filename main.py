fb050b0ed839cdd1ee7e709413d407832746bf18
Matthew Black <matthewallenblack@gmail.com>
2024-06-12 12:37:40 -0500

    add comment about FastAPI's Starlette inheritance

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # https://fastapi.tiangolo.com/tutorial/first-steps/
 
 from fastapi import FastAPI
+# Note: FastAPI is a class that inherits directly from Starlette:
+  # https://www.starlette.io
 
 app = FastAPI()
 
