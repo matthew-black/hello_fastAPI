503d0fba906ef83ecea867a1f061694f923674e0
Matthew Black <matthewallenblack@gmail.com>
2024-06-13 15:15:37 -0500

    example of route params with type-checking

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 from fastapi import FastAPI
 # Note: FastAPI is a class that inherits directly from Starlette:
-  # https://www.starlette.io
+    # https://www.starlette.io
 
 app = FastAPI()
 
@@ -10,3 +10,10 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello FastAPI"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    # If the item_id param can't be parsed as an int, this route
+    # will respond with 422 and { detail: [ {errorObject} ] }
+    return {"item_id": item_id}
+
